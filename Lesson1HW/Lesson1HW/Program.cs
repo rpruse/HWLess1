906fdaca83930efce8fcf0ff9e786fcdb0b2213a
rudite.pruse@gmail.com <rudite.pruse@gmail.com>
2021-09-03 22:01:29 +0300

    My Homework published

---
+++ Lesson1HW/Lesson1HW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson1HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
